d49f36e6a05016ccc9ce649eb7c49d7a0390326d
ticavin <606740@gmail.com>
2022-05-25 21:27:23 +0300

    fix1

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,7 +5,7 @@ const folder = path.join(__dirname, 'secret-folder');
 async function info(item) {
   const ffff = path.join(folder, item);
   const name = path.basename(item, path.extname(item));
-  const ex = path.exname(item);
+  const ex = path.extname(item);
   fs.stat(ffff, (error, data) => {
     const size =Math.floor((data.size / 1024) * 100) / 100;
     console.log(`${name} - ${ex.slice(1)} - ${size}KB`);
